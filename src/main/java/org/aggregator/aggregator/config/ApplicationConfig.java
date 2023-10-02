62cb36f77b7c5b18b6f9124c5cb234c03ba3fdc7
Святослав Киреев <svytkireev@icloud.com>
2023-10-02 20:32:07 +0700

    Добавлен токен и добавлена регистрация авторизация, но проверить не могу, тк проект не запускается! видится нужно колдавать с вот этой аннотацией @EnableJpaRepositories

--- src/main/java/org/aggregator/aggregator/config/ApplicationConfig.java
+++ src/main/java/org/aggregator/aggregator/config/ApplicationConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableJpaRepositories()
 public class ApplicationConfig {
     private final UserRepository userRepository;
 
